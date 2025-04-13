6e6cd70ebbac7d198e673bec7305151db83e6e03
Saifazom <saifazam@Saifazoms-MacBook-Pro.local>
2025-04-13 21:34:53 +0600

    Update header component and styles

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router";
+import siteLogo from "../../assets/img/site-logo.svg";
 
 export default function Header() {
   return (
-    <header className="border-b">
-      <div className="container flex h-16 items-center justify-between mx-auto px-4">
-        <Link to="/" className="text-xl font-bold">
-          React Starter
+    <header className="mt-10">
+      <div className="container lg:max-xl:flex flex h-16 items-center justify-between mx-auto px-4">
+        <Link to="/">
+          <img src={siteLogo} alt="Site Logo" />
         </Link>
+
         <nav className="gap-4">
           <ul className="flex">
             <li>
               <Link to="/" className="hover:text-primary">
-                Home page
+                Home
               </Link>
             </li>
             <li>
@@ -26,6 +28,10 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+
+        <button className="lg:text-[100px] md:text-[50px] sm:text-[30px] text-[10px] bg-black text-white">
+          Let's Talk
+        </button>
       </div>
     </header>
   );
