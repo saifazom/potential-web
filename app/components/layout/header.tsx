446daa3b3af779a9cc5c060b82abd9fea6eb4a74
Saifazom <saifazam@Saifazoms-MacBook-Pro.local>
2025-04-15 03:45:45 +0600

    Update header component and styles

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -3,35 +3,169 @@ import siteLogo from "../../assets/img/site-logo.svg";
 
 export default function Header() {
   return (
-    <header className="mt-10">
-      <div className="container lg:max-xl:flex flex h-16 items-center justify-between mx-auto px-4">
-        <Link to="/">
-          <img src={siteLogo} alt="Site Logo" />
-        </Link>
+    <header>
+      <div className="container mx-auto relative py-6 mt-4 ">
+        <div className="flex-items-center justify-between px-4">
+          <Link to="/">
+            <img src={siteLogo} alt="Site Logo" />
+          </Link>
 
-        <nav className="gap-4">
-          <ul className="flex">
-            <li>
-              <Link to="/" className="hover:text-primary">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="hover:text-primary">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/login" className="hover:text-primary">
-                Login
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className="max-lg:hidden">
+            <ul>
+              <li>
+                <Link to="/services">Services</Link>
 
-        <button className="lg:text-[100px] md:text-[50px] sm:text-[30px] text-[10px] bg-black text-white">
-          Let's Talk
-        </button>
+                <div className="u-dropdown">
+                  <ul>
+                    <li>
+                      <Link to="/e-commerce">
+                        <span>01</span> E-commerce
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/healthcare">
+                        <span>02</span> Healthcare
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/education">
+                        <span>03</span> Education
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/software">
+                        <span>04</span> Software
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/real-estate">
+                        <span>05</span> Real Estate
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/logistics-&-fulfilment">
+                        <span>06</span> Logistics & Fulfilment
+                      </Link>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <Link to="/transport-&-travel">
+                        <span>07</span> Transport & Travel
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/startup">
+                        <span>08</span> Startup
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/automotive">
+                        <span>09</span> Automotive
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/services">
+                        <span>10</span> Services
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/printing-services">
+                        <span>11</span> Printing Services
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/e-commerce">
+                        <span>12</span> E-commerce
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <Link to="/about-us">About Us</Link>
+
+                <div className="u-dropdown">
+                  <ul>
+                    <li>
+                      <Link to="/e-commerce">
+                        <span>01</span> E-commerce
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/healthcare">
+                        <span>02</span> Healthcare
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/education">
+                        <span>03</span> Education
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/software">
+                        <span>04</span> Software
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/real-estate">
+                        <span>05</span> Real Estate
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/logistics-&-fulfilment">
+                        <span>06</span> Logistics & Fulfilment
+                      </Link>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <Link to="/transport-&-travel">
+                        <span>07</span> Transport & Travel
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/startup">
+                        <span>08</span> Startup
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/automotive">
+                        <span>09</span> Automotive
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/services">
+                        <span>10</span> Services
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/printing-services">
+                        <span>11</span> Printing Services
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/e-commerce">
+                        <span>12</span> E-commerce
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/career">Career</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <button className="u-button-violate">Let's Talk</button>
+        </div>
       </div>
     </header>
   );
